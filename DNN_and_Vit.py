2ddc9e696c4c350501098dddad64c5826bc28dec
jj <jakob.scheytt@gmx.de>
2023-11-03 15:20:31 +0100

    Created combination of DNN and Viterbi

---
+++ DNN_and_Vit.py
@@ -0,0 +1,16 @@
+class DnnAndVit:
+
+    def __init__(self, dataset="Sleep-EDF-2013", fold=1, nr=0, used_set="train", trans_matr="edf-2013-and-edf-2018"):
+        self.dataset = dataset
+        self.fold = fold
+        self.nr = nr
+        self.used_set = used_set
+        self.trans_matr = trans_matr
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
