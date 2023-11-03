e43a787d50fc8b68ee4e2b500791aba0d975d2d5
jj <jakob.scheytt@gmx.de>
2023-11-03 09:08:00 +0100

    Method -> class

--- Viterbi/Adapted_Stack_Overflow.py
+++ Viterbi/Adapted_Stack_Overflow.py
@@ -1,116 +1,143 @@
-#Source: https://stackoverflow.com/questions/9729968/python-implementation-of-viterbi-algorithm checked 05th Oct 2023 - adapted
+    #Source: https://stackoverflow.com/questions/9729968/python-implementation-of-viterbi-algorithm checked 05th Oct 2023 - adapted
 import numpy as np
 
-def viterbi(A, P, Pi=None, logscale = False):
+class Viterbi:
     """
-    Return the MAP estimate of state trajectory of Hidden Markov Model.
+            Return the MAP estimate of state trajectory of Hidden Markov Model.
+            Creates an object with containing the best bath.
 
-    Parameters
-    ----------
-    A : array (K, K)
-        State transition matrix. See HiddenMarkovModel.state_transition  for
-        details. Example: [[1->1, 1->2],[2->1, 2->2]]
-    P: array(T, K)
-        Probability of being of state k given observation in time t - calculated via DNN.
-        Example: [[Prob. of 0.3 to be in state 1 at time 0 given the data, Prob of 0.7 to be in state 2 at t = 0],
-        [prob of 0.4 for state 1 at time 1, prob of 0.6 for state 2 at time 2]]
-    Pi: optional, (K,)
-        Initial state probabilities: Pi[i] is the probability x[0] == i. If
-        None, uniform initial distribution is assumed (Pi[:] == 1/K).
-    logscale: optional, (bool)
-        Defines whether the calculation is logarithmic or not. Default is False; if true, T1 will contain logarithmic
-        probabilities
-
-
-
-    Returns
-    -------
-    x : array (T,)
-        Maximum a posteriori probability estimate of hidden state trajectory,
-        conditioned on observation sequence y under the model parameters A, B,
-        Pi.
-        Example: [0 0 1] means that the most likely a posteriori path is state 0, state 0, state 1
-    T1: array (K, T)
-        the probability of the most likely path so far
-        Example:
-        [[0.3     0.084   0.00588]
-       [0.04    0.027   0.01512]]
-       means prob. of 0.3 to be in State 0 at time 0 and  prob. of 0.04 to be in Staate 1 at t=0;  prob. of 0.084 to be
-       in State 0 at t=1, (c.f. T2 to find the most likely path to get there);
-       prob of 0.027 to be in State 1 at time t=1 (c.f. T2 to find the most likely path to get there)
-        the x_j-1 of the most likely path so far
-    """
-    # Cardinality of the state space
-    K = A.shape[0]
-    # Initialize the priors with default (uniform dist) if not given by caller
-
-    Pi = Pi if Pi is not None else np.full(K, 1 / K)
-    T = len(P)
-
-    if logscale:
-        A = np.log(A)
-        P = np.log(P)
-        Pi = np.log(Pi)
-
-    T1 = np.empty((K, T), 'd')
-    T2 = np.empty((K, T), 'B')
-
-    # Initialize the tracking tables from first observation
-    #T1[:, 0] = Pi * B[:, y[0]]
-    #print("B[:, y[0]]: \n", B[:, y[0]], "\n")
-
-    if logscale:
-        T1[:, 0] = Pi + P[0]
-
-    else:
-        T1[:, 0] = Pi * P[0]
+            Inputs:
+            ----------
+            A : array (K, K)
+                State transition matrix. See HiddenMarkovModel.state_transition  for
+                details. Example: [[1->1, 1->2],[2->1, 2->2]]
+            P: array(T, K)
+                Probability of being of state k given observation in time t - calculated via DNN.
+                Example: [[Prob. of 0.3 to be in state 1 at time 0 given the data, Prob of 0.7 to be in state 2 at t = 0],
+                [prob of 0.4 for state 1 at time 1, prob of 0.6 for state 2 at time 2]]
+            Pi: optional, (K,)
+                Initial state probabilities: Pi[i] is the probability x[0] == i. If
+                None, uniform initial distribution is assumed (Pi[:] == 1/K).
+            logscale: optional, (bool)
+                Defines whether the calculation is logarithmic or not. Default is False; if true, T1 will contain logarithmic
+                probabilities
 
 
 
-    T2[:, 0] = 0
-    #print("T1: \n", T1,"\n \n T2: \n ", T2, "\n\n")
+            Calculates:
+            -------
+            x : array (T,)
+                Maximum a posteriori probability estimate of hidden state trajectory,
+                conditioned on observation sequence y under the model parameters A, B,
+                Pi.
+                Example: [0 0 1] means that the most likely a posteriori path is state 0, state 0, state 1
+            T1: array (K, T)
+                the probability of the most likely path so far
+                Example:
+                [[0.3     0.084   0.00588]
+               [0.04    0.027   0.01512]]
+               means prob. of 0.3 to be in State 0 at time 0 and  prob. of 0.04 to be in Staate 1 at t=0;  prob. of 0.084 to be
+               in State 0 at t=1, (c.f. T2 to find the most likely path to get there);
+               prob of 0.027 to be in State 1 at time t=1 (c.f. T2 to find the most likely path to get there)
+                the x_j-1 of the most likely path so far
+            """
+    def __init__(self, A, P, Pi=None, logscale = False):
+        self.A = A
+        self.P = P
+        self.logscale = logscale
 
-    # Iterate through the observations updating the tracking tables
-    for i in range(1, T):
-        if logscale:
-            T1[:, i] = np.max(T1[:, i - 1] + A.T + (P[np.newaxis, 1]).T,1)  # addiere Wkt des letzten States
-            # mit Transitionswahrscheinlichkeit mit Wkt für den aktuellen Zustand aus DNN; suche den State aus
-            # vorheriger Periode, der Wkt maximiert
-            T2[:, i] = np.argmax(T1[:, i - 1] + A.T, 1)
+        # Cardinality of the state space
+        self.K = A.shape[0]
+        # Initialize the priors with default (uniform dist) if not given by caller
+        self.Pi = Pi if Pi is not None else np.full(self.K, 1 / self.K)
+
+        self.T = len(P)
+
+        if self.logscale:
+            self.A = np.log(self.A)
+            self.P = np.log(self.P)
+            self.Pi = np.log(self.Pi)
+
+        (self.x, self.T1, self.T2) = self.calc_viterbi()
+
+
+    def calc_viterbi(self):
+
+
+
+        T1 = np.empty((self.K, self.T), 'd')
+        T2 = np.empty((self.K, self.T), 'B')
+
+        # Initialize the tracking tables from first observation
+        #T1[:, 0] = Pi * B[:, y[0]]
+        #print("B[:, y[0]]: \n", B[:, y[0]], "\n")
+
+        if self.logscale:
+            T1[:, 0] = self.Pi + self.P[0]
 
         else:
-            T1[:, i] = np.max(T1[:, i - 1] * A.T * (P[np.newaxis, 1]).T, 1) # multipliziere Wkt des letzten States
-            # mit Transitionswahrscheinlichkeit mit Wkt für den aktuellen Zustand aus DNN; suche den State aus
-            # vorheriger  Periode, der Wkt maximiert
-            T2[:, i] = np.argmax(T1[:, i - 1] * A.T, 1)
-        # print("\n i:",i, "\nT1: \n", T1,"\n \n T2: \n ", T2, "\n\n")
-
-    # Build the output, optimal model trajectory
-    x = np.empty(T, 'B')
-    x[-1] = np.argmax(T1[:, T - 1])
-    for i in reversed(range(1, T)):
-        x[i - 1] = T2[x[i], i]
-
-    #if logscale:
-    #    T1 = np.exp(T1)
-
-    return x, T1, T2
-
-#observations: normal = 0, cold = 1, dizzy = 2
-#states: Healthy = 0, Fever = 1
-#y = np.array([0, 1, 2]) #observation state sequence; expl.: observations are normal(0) then cold (1) then dizzy (2)
-A = np.array([[0.7, 0.3], [0.9, 0.1]]) #state transition matrix: e.g. prob of 0.7 to transition from Healthy to Healthy,
-# prob of 0.3 to transition from healthy to fever
-#B = np.array([[0.5, 0.4, 0.1],[0.1, 0.3, 0.6]]) #Emission matrix: e.g. prob of 0.5 to feel normal if you're healthy,
-# prob of 0.4 to feel cold when healthy
-Pi = np.array([0.8, 0.2]) #initial distribution, expl.: prob. of 0.6 to start healthy, prob of 0.4 to start ill
-P = np.array([[.5, .5], [0.2, 0.8], [0.4, 0.6]]) #e.g. DNN has calculated that - given the data - there's a 30% chance
-# that the first state is Healthy (shape 3,2)
+            T1[:, 0] = self.Pi * self.P[0]
 
 
-x_1, T1_1, T2_1 = viterbi(A, P, Pi, False)
-x_2, T1_2, T2_2 = viterbi(A, P, Pi, True)
-print(x_1 == x_2, np.round(T1_1, 4) == np.round(np.exp(T1_2),4), T2_1 == T2_2)
-print(x_1)
 
-##maybe add normalization?
+        T2[:, 0] = 0
+        #print("T1: \n", T1,"\n \n T2: \n ", T2, "\n\n")
+
+        # Iterate through the observations updating the tracking tables
+        for i in range(1, self.T):
+            if self.logscale:
+                T1[:, i] = np.max(T1[:, i - 1] + self.A.T + (self.P[np.newaxis, 1]).T,1)  # addiere Wkt des letzten States
+                # mit Transitionswahrscheinlichkeit mit Wkt für den aktuellen Zustand aus DNN; suche den State aus
+                # vorheriger Periode, der Wkt maximiert
+                T2[:, i] = np.argmax(T1[:, i - 1] + self.A.T, 1)
+
+            else:
+                T1[:, i] = np.max(T1[:, i - 1] * self.A.T * (self.P[np.newaxis, 1]).T, 1) # Add the probability of the last state's occurrence
+                # to the transition probability with the probability for the current state from the DNN (Deep Neural Network).
+                # Find the state from the previous period that maximizes the probability.
+
+                T2[:, i] = np.argmax(T1[:, i - 1] * self.A.T, 1)
+            # print("\n i:",i, "\nT1: \n", T1,"\n \n T2: \n ", T2, "\n\n")
+
+        # Build the output, optimal model trajectory
+        x = np.empty(self.T, 'B')
+        x[-1] = np.argmax(T1[:, self.T - 1])
+        for i in reversed(range(1, self.T)):
+            x[i - 1] = T2[x[i], i]
+
+        #if logscale:
+        #    T1 = np.exp(T1)
+
+        return x, T1, T2
+
+    """#observations: normal = 0, cold = 1, dizzy = 2
+    #states: Healthy = 0, Fever = 1
+    #y = np.array([0, 1, 2]) #observation state sequence; expl.: observations are normal(0) then cold (1) then dizzy (2)
+    A = np.array([[0.7, 0.3], [0.9, 0.1]]) #state transition matrix: e.g. prob of 0.7 to transition from Healthy to Healthy,
+    # prob of 0.3 to transition from healthy to fever
+    #B = np.array([[0.5, 0.4, 0.1],[0.1, 0.3, 0.6]]) #Emission matrix: e.g. prob of 0.5 to feel normal if you're healthy,
+    # prob of 0.4 to feel cold when healthy
+    Pi = np.array([0.8, 0.2]) #initial distribution, expl.: prob. of 0.6 to start healthy, prob of 0.4 to start ill
+    P = np.array([[.5, .5], [0.2, 0.8], [0.4, 0.6]]) #e.g. DNN has calculated that - given the data - there's a 30% chance
+    # that the first state is Healthy (shape 3,2)"""
+
+
+    ##maybe add normalization?
+
+def main():
+    A = np.array([[0.7, 0.3], [0.9, 0.1]])
+    Pi = np.array([0.8, 0.2])
+    P = np.array([[.5, .5], [0.2, 0.8], [0.4, 0.6]])
+
+    Viterbi_1 = Viterbi(A, P, Pi, False)
+    Viterbi_2 = Viterbi(A, P, Pi, True)
+
+    x_1, T1_1, T2_1 = Viterbi_1.x, Viterbi_1.T1, Viterbi_1.T2
+    x_2, T1_2, T2_2 = Viterbi_2.x, Viterbi_2.T1, Viterbi_2.T2
+
+    print(x_1 == x_2, np.round(T1_1, 4) == np.round(np.exp(T1_2), 4), T2_1 == T2_2)
+    print(x_1)
+
+
+if __name__ == "__main__":
+    main()
