bc2e6c5a0fb7c4c671d0760ef4c084709dc05324
jj <jakob.scheytt@gmx.de>
2023-11-13 15:35:04 +0100

    Added option for logscale

--- DNN_and_Vit.py
+++ DNN_and_Vit.py
@@ -4,7 +4,7 @@ from Viterbi.Viterbi_Algorithm import *
 class DnnAndVit:
 
     def __init__(self, dataset="Sleep-EDF-2013", fold=1, nr=0, used_set="train", trans_matr="edf-2013-and-edf-2018",
-                 length=None, start=-1):
+                 length=None, start=None, logscale = False):
         self.P_Matrix_labels = None
         self.P_Matrix_probs = None
         self.start = None
@@ -15,6 +15,7 @@ class DnnAndVit:
         self.used_set = used_set
         self.trans_matr = trans_matr
         self.start = start
+        self.logscale = logscale
 
         self.Transition_Matrix = self.load_Transition_Matrix()
         self.load_P_Matrix()
@@ -23,7 +24,7 @@ class DnnAndVit:
         self.set_length(length)
         self.set_start(start)
 
-        self.selected_P_Matrix = self.selected_P_Matrix()
+        self.selected_P_Matrix()
 
         self.hybrid_predictions = self.hybrid_predictions()
 
@@ -34,38 +35,45 @@ class DnnAndVit:
 
     def load_P_Matrix(self):
         P_path = (
-                    "./Probability_Data/" + self.dataset + "/_dataset_" + self.dataset + "_set_" + self.used_set + "_fold_" + str(
-                self.fold) + "_nr_" + str(self.nr))
+                "./Probability_Data/" + self.dataset + "/_dataset_" + self.dataset + "_set_" + self.used_set + "_fold_" + str(
+            self.fold) + "_nr_" + str(self.nr))
 
-        self.P_Matrix_labels = np.loadtxt(P_path + "_labels.txt", delimiter=",")
+        self.P_Matrix_labels = np.loadtxt(P_path + "_labels.txt", delimiter=",", dtype=int)
         self.P_Matrix_probs = np.loadtxt(P_path + "_probs.txt", delimiter=",")
 
     def pure_predictions(self):
         return np.argmax(self.P_Matrix_probs, axis=1)
 
     def set_length(self, length):
-        if (length is None):
+        if length is None:
             self.length = len(self.P_Matrix_labels)
-        elif ((length <= len(self.P_Matrix_labels)) and length > 0):
+        elif (length <= len(self.P_Matrix_labels)) and length > 0:
             self.length = length
 
-        elif (length > len(self.P_Matrix_labels)):
+        elif length > len(self.P_Matrix_labels):
             l = len(self.P_Matrix_labels)
             print("Length is higher than data length. Length is set to the length of the input file: " + str(l))
             self.length = l
         else:
             self.length = len(self.P_Matrix_labels)
-            print("Length is negative or zero. Length is set to the length of the input file:" + str(self.length))
+            print("Length is negative or zero. Length is set to the length of the input file: " + str(self.length))
 
     def set_start(self, start):
-        if (start < 0 and self.length == len(self.P_Matrix_labels)):
+        if (start is None) and (self.length == len(self.P_Matrix_labels)):
+            print("Start index is not defined and length is equal to input length; start index is set to 0")
+            self.start = 0
+        elif start is None:
+            self.start = np.random.randint(0, (len(self.P_Matrix_labels)) - self.length)
+        elif start < 0 and self.length == len(self.P_Matrix_labels):
             print("Start index is negative and the length is equal to input length; Start index is set to 0")
             self.start = 0
-        elif (start < 0):
+        elif start < 0:
             self.start = np.random.randint(0, (len(self.P_Matrix_labels)) - self.length)
-        elif (start > len(self.P_Matrix_labels) - self.length):
+        elif start > len(self.P_Matrix_labels) - self.length:
             self.start = len(self.P_Matrix_labels) - self.length
-            print("Start index + length is higher than data length. Start index is set to the lowest possible value: "+str(self.start))
+            print(
+                "(Start index + length) is higher than data length. Start index is set to the lowest possible value: " + str(
+                    self.start))
         else:
             self.start = start
 
@@ -77,14 +85,16 @@ class DnnAndVit:
         self.pure_predictions = self.pure_predictions[beg:end]
 
     def hybrid_predictions(self):
-        vit = Viterbi(self.Transition_Matrix, self.P_Matrix_probs)
+        vit = Viterbi(A = self.Transition_Matrix, P = self.P_Matrix_probs, logscale=self.logscale)
         result = vit.x
-        print(result)
+        return result
 
 
 def main():
-    dnn2=DnnAndVit(length=20)
-    print(dnn2.start, dnn2.length)
+    dnn2 = DnnAndVit(length=None, logscale=True)
+    print("Start: ", dnn2.start, " length: ", dnn2.length)
+    print("Labels: \t \t \t", dnn2.P_Matrix_labels, "\nSleePyCo Prediction:", dnn2.pure_predictions, "\nHybrid Prediction:\t",
+          dnn2.hybrid_predictions)
 
 
 if __name__ == '__main__':
