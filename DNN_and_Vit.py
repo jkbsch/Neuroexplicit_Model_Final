Now implemented: replacement of argmax with softmax and MSELoss - not yet correct

DNN_and_Vit.py
@@ -35,11 +35,17 @@ class DnnAndVit:
 
         self.selected_P_Matrix()
 
-        self.hybrid_predictions, self.hybrid_probs = self.hybrid_predictions()
+        if torch.is_tensor(self.Transition_Matrix):
+            self.hybrid_predictions, self.hybrid_probs, self.hybrid_softmax = self.hybrid_predictions()
+        else:
+            self.hybrid_predictions, self.hybrid_probs = self.hybrid_predictions()
 
 
         self.korrekt_SleePy = np.sum(self.P_Matrix_labels == self.pure_predictions)
         self.korrekt_hybrid = np.sum(self.P_Matrix_labels == self.hybrid_predictions)
+
+        if torch.is_tensor(self.Transition_Matrix):
+            self.compare_softmax_argmax = np.sum(self.hybrid_predictions == np.round((self.hybrid_softmax.detach().cpu().numpy())))
 
 
 
@@ -97,7 +103,10 @@ class DnnAndVit:
         logscale=self.logscale, alpha=self.alpha, print_info=self.print_info) return vit.x.numpy()"""
         vit = Viterbi(A=self.Transition_Matrix, P=self.P_Matrix_probs, logscale=self.logscale, alpha=self.alpha,
                       print_info=self.print_info)
-        return vit.x, vit.T1
+        if torch.is_tensor(self.Transition_Matrix):
+            return vit.x, vit.T1, vit.y
+        else:
+            return vit.x, vit.T1
 
 
 def main():

Optimize_Transmatrix.py
@@ -6,8 +6,8 @@ from Viterbi.Viterbi_Algorithm import *
 
 class OptimTransMatrix:
     def __init__(self, dataset='Sleep-EDF-2013', checkpoints='given', trans_matrix='EDF_2013', fold=1, num_epochs=2,
-                 learning_rate=0.0000001, alpha=None, train_transition=True, train_alpha=False, save=False,
-                 print_info=True, print_results=False, save_unsuccesful=False, use_normalized=True):
+                 learning_rate=0.0001, alpha=None, train_transition=True, train_alpha=False, save=False,
+                 print_info=True, print_results=False, save_unsuccesful=False, use_normalized=True, softmax=False):
 
         # Device configuration
         # torch.autograd.set_detect_anomaly(True)
@@ -27,6 +27,7 @@ class OptimTransMatrix:
         self.no_nan = True
         self.save_unsuccesful = save_unsuccesful
         self.use_normalized = use_normalized
+        self.softmax = softmax
 
         self.TestDataset = self.TestSleepDataset(self.device, self.dataset, self.checkpoints, self.trans_matrix,
                                                  self.fold)
@@ -37,8 +38,11 @@ class OptimTransMatrix:
 
         self.learning_rate = learning_rate
         self.num_epochs = num_epochs
-
-        self.loss = nn.CrossEntropyLoss()
+        if self.softmax:
+            # self.loss = nn.KLDivLoss(reduction='batchmean')
+            self.loss = nn.MSELoss()
+        else:
+            self.loss = nn.CrossEntropyLoss()
 
         train_params = []
         if train_transition:
@@ -140,14 +144,17 @@ class OptimTransMatrix:
 
         else:
             res_normalized = None
-        return res.x, res.T1, res_normalized
+        # print(f"Percentage where x == y: {np.sum(res.x.detach().cpu().numpy() == np.round((res.y.detach().cpu().numpy())))/len(res.x)} ")
+        return res.x, res.T1, res_normalized, res.y
 
     def train(self, epoch):
         nr, total_loss, total_acc = 0, 0, 0
         for i, (inputs, targets) in enumerate(self.train_loader):
             inputs = torch.squeeze(inputs, dim=0) # vlt Fehler wegen log von 0?
             targets = torch.squeeze(targets, dim=0)
-            labels_predicted, y_predicted_unnormalized, y_predicted_normalized = self.forward(inputs)
+            if self.softmax:
+                targets = targets.to(dtype=torch.float64)
+            labels_predicted, y_predicted_unnormalized, y_predicted_normalized, res_softmax = self.forward(inputs)
             labels_predicted = labels_predicted.to(dtype=torch.int64)
             one_hot = (nn.functional.one_hot(labels_predicted, 5)).to(dtype=torch.float64)
             """if i % 10 == 0:
@@ -157,7 +164,11 @@ class OptimTransMatrix:
                 pred = y_predicted_normalized
             else:
                 pred = y_predicted_unnormalized
-            loss = self.loss(torch.transpose(pred, 0, 1), targets)
+            if self.softmax:
+                loss = self.loss(torch.log(res_softmax), targets)
+            else:
+                loss = self.loss(torch.transpose(pred, 0, 1), targets)
+
             ### Loss jetzt mal mit Predicted Labels - funktioniert noch nicht aber vlt. könnte man das implementieren
             # loss = self.loss(torch.transpose(nn.functional.one_hot(labels_predicted), 0, 1), targets)
             # loss = self.loss(torch.clamp(nn.functional.one_hot(labels_predicted), min=float(1e-12)), targets)
@@ -176,7 +187,7 @@ class OptimTransMatrix:
 
             # zero the gradients after updating
             self.optimizer.zero_grad()
-            if (epoch % 10 == 0 or epoch == self.num_epochs-1) and self.print_results:
+            if (epoch % 1 == 0 or epoch == self.num_epochs-1) and self.print_results:
                 total_acc += (labels_predicted == targets).sum().item() / len(labels_predicted)
                 total_loss += loss.item()
                 nr += 1
@@ -195,22 +206,26 @@ class OptimTransMatrix:
         return True
 
     def test(self, epoch):
-        if epoch % 10 == 0 or epoch == self.num_epochs-1:
+        if epoch % 1 == 0 or epoch == self.num_epochs-1:
             test_loss, correct, nr = 0, 0, 0
             with torch.no_grad():
                 for i, (inputs, targets) in enumerate(self.test_loader):
                     inputs = torch.squeeze(inputs, dim=0)
                     targets = torch.squeeze(targets, dim=0)
-                    labels_predicted, y_predicted_unnormalized, y_predicted_normalized = self.forward(inputs)
+                    labels_predicted, y_predicted_unnormalized, y_predicted_normalized, res_softmax = self.forward(inputs)
 
                     labels_predicted = labels_predicted.to(dtype=torch.int64)
-                    one_hot = nn.functional.one_hot(labels_predicted, 5).to(dtype=torch.float64)
+                    # one_hot = nn.functional.one_hot(labels_predicted, 5).to(dtype=torch.float64)
 
                     if self.use_normalized:
                         pred = y_predicted_normalized
                     else:
                         pred = y_predicted_unnormalized
-                    test_loss += self.loss(torch.transpose(pred, 0, 1), targets).item()
+                    if self.softmax:
+                        loss = self.loss(torch.log(res_softmax), targets)
+                    else:
+                        loss = self.loss(torch.transpose(pred, 0, 1), targets)
+                    test_loss += loss.item()
                     #test_loss += self.loss(one_hot, targets)
                     correct += (labels_predicted == targets).sum().item() / len(labels_predicted)
                     nr += 1
@@ -281,9 +296,9 @@ class OptimTransMatrix:
 
 
 def main():
-    OptimTransMatrix(dataset='Sleep-EDF-2013', num_epochs=60, learning_rate=0.01, print_results=True,
-                     train_alpha=False, train_transition=True, alpha=0.3, fold=1, save=False,
-                     save_unsuccesful=False, use_normalized=False)
+    OptimTransMatrix(dataset='Sleep-EDF-2013', num_epochs=60, learning_rate=0.001, print_results=True,
+                     train_alpha=True, train_transition=False, alpha=0.5, fold=1, save=False,
+                     save_unsuccesful=False, use_normalized=False, softmax=True)
     """for alpha in [0.3, 0.5]:
         for fold in range(1, 21):
             print(f'Sleep-EDF-2013, 60 epochs, lr = 0.0001, train_alpha = True, train_transition = True, alpha = {alpha}, fold={fold}')

Viterbi/Viterbi_Algorithm.py
@@ -28,19 +28,23 @@ class Viterbi:
             c.f. T2 to find the most likely path to get there) the x_j-1 of the most likely path so far
     """
 
+
     def __init__(self, A, P, Pi=None, logscale=True, alpha=None, return_log=None, print_info=True):
+        # check if the input is a torch tensor or a numpy array
         if torch.is_tensor(A):
             self.is_torch = True
             self.device = A.device
         else:
             self.is_torch = False
 
+        # Initialize the model given the parameters
         self.A = A
         #self.A[A<0] = 0
         self.P = P
         self.logscale = logscale
         self.print_info = print_info
 
+        # Initialize the return_log variable
         if return_log is None:
             self.return_log = self.logscale
         else:
@@ -54,10 +58,11 @@ class Viterbi:
         else:
             self.Pi = Pi if Pi is not None else np.full(self.K, 1 / self.K)
 
+
         self.alpha = self.alpha(alpha)
 
         self.T = len(P)
-
+        # convert to logscale if needed
         if self.logscale:  # convert to logscale
             if self.is_torch:
                 self.A = torch.log(self.A)
@@ -68,8 +73,12 @@ class Viterbi:
                 self.P = np.log(self.P)
                 self.Pi = np.log(self.Pi)
 
-        (self.x, self.T1, self.T2) = self.calc_viterbi()
+        if self.is_torch:
+            self.x, self.T1, self.T2, self.T3, self.y = self.calc_viterbi()
+        else:
+            (self.x, self.T1, self.T2) = self.calc_viterbi()
 
+    # check if alpha is None and set it to 0.5 if it is
     def alpha(self, alpha):
         if alpha is None:
             if self.is_torch:
@@ -84,11 +93,14 @@ class Viterbi:
         else:
             return alpha
 
+    # Calculate the most likely state trajectory using the Viterbi algorithm
     def calc_viterbi(self):
 
+        # Initialize the tracking tables
         if self.is_torch:
             T1 = torch.empty((self.K, self.T), dtype=torch.float64, device=self.device)
             T2 = torch.empty((self.K, self.T), dtype=torch.int, device=self.device)
+            T3 = torch.zeros((self.K, self.T), dtype=torch.float64, device=self.device)
         else:
             T1 = np.empty((self.K, self.T), 'd')
             T2 = np.empty((self.K, self.T), 'B')
@@ -112,7 +124,9 @@ class Viterbi:
                     T1[:, i] = torch.max(
                         T1[:, i - 1] + 2 * self.alpha * self.A.T + 2 * (1 - self.alpha) * (self.P[None, i]).T, 1).values
                     T2[:, i] = torch.argmax(T1[:, i - 1] + 2 * self.alpha * self.A.T, 1)
-                    # T2[:, i] = torch.nn.Softmax(T1[:, i - 1] + 2 * self.alpha * self.A.T, 1)
+                    temp = torch.nn.functional.softmax(T1[:, i - 1] + 2 * self.alpha * self.A.T, 1)
+                    T3[:, i] = torch.matmul(temp, torch.arange(self.K, dtype=torch.float64)[:,None]).squeeze(1)
+
                 else:
                     T1[:, i] = np.max(
                         T1[:, i - 1] + 2 * self.alpha * self.A.T + 2 * (1 - self.alpha) * (self.P[np.newaxis, i]).T, 1)
@@ -138,15 +152,19 @@ class Viterbi:
         # Build the output, optimal model trajectory
         if self.is_torch:
             x = torch.empty(self.T, dtype=torch.int64, device=self.device)
+            y = torch.empty(self.T, dtype=torch.float64, device=self.device)
             # x = torch.empty(self.T, dtype=torch.float64, device=self.device)
             x[-1] = torch.argmax(T1[:, self.T - 1])
-            # x[-1] = torch.nn.Softmax(T1[:, self.T - 1])
+            temp = torch.nn.functional.softmax(T1[:, self.T - 1], dim=0)
+            y[-1] = torch.matmul(temp, torch.arange(self.K, dtype=torch.float64)[:,None])
         else:
             x = np.empty(self.T, 'B')
             x[-1] = np.argmax(T1[:, self.T - 1])
 
         for i in reversed(range(1, self.T)):
             x[i - 1] = T2[x[i], i]
+            if self.is_torch:
+                y[i - 1] = T3[x[i], i]
 
         if self.logscale != self.return_log:
             if self.return_log:
@@ -160,7 +178,10 @@ class Viterbi:
                 else:
                     T1 = np.exp(T1)
 
-        return x, T1, T2
+        if self.is_torch:
+            return x, T1, T2, T3, y
+        else:
+            return x, T1, T2
 
     """#observations: normal = 0, cold = 1, dizzy = 2 #states: Healthy = 0, Fever = 1 #y = np.array([0, 1, 
     2]) #observation state sequence; expl.: observations are normal(0) then cold (1) then dizzy (2) A = np.array([[
@@ -175,14 +196,14 @@ class Viterbi:
 def main():
     A = np.array([[0.7, 0.3], [0.9, 0.1]])
     Pi = np.array([0.8, 0.2])
-    P = np.array([[.3, .3], [0.8, 0.3], [0.4, 0.6]])
+    P = np.array([[0.1, .9], [0.9, 0.1], [0.01, 0.99]])
 
     A1 = torch.from_numpy(A).to(dtype=torch.float64)
     Pi1 = torch.from_numpy(Pi).to(dtype=torch.float64)
     P1 = torch.from_numpy(P).to(dtype=torch.float64)
 
     Viterbi_1 = Viterbi(A, P, Pi, logscale=True, return_log=True)
-    Viterbi_2 = Viterbi(A1, P1, Pi1, logscale=False, return_log=True)
+    Viterbi_2 = Viterbi(A1, P1, Pi1, logscale=True, return_log=True)
 
     x_1, T1_1, T2_1 = Viterbi_1.x, Viterbi_1.T1, Viterbi_1.T2
     x_2, T1_2, T2_2 = Viterbi_2.x.numpy(), Viterbi_2.T1.numpy(), Viterbi_2.T2.numpy()