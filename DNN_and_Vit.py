Now implemented: replacement of argmax with softmax and MSELoss - not yet correct

DNN_and_Vit.py
@@ -5,7 +5,7 @@ from HMM_utils import *
 class DnnAndVit:
 
     def __init__(self, dataset="Sleep-EDF-2013", fold=1, nr=0, used_set="train", trans_matr="edf-2013-and-edf-2018",
-                 length=None, start=None, logscale=True, alpha=None, print_info=True, checkpoints='given'):
+                 length=None, start=None, logscale=True, alpha=None, print_info=True, checkpoints='given', softmax=False):
         self.P_Matrix_labels = None
         self.P_Matrix_probs = None
         self.start = None
@@ -20,6 +20,7 @@ class DnnAndVit:
         self.alpha = alpha
         self.print_info = print_info
         self.checkpoints = checkpoints
+        self.softmax = softmax
 
         if type(trans_matr) == str:
             self.Transition_Matrix = load_Transition_Matrix(trans_matr)[1]
@@ -102,7 +103,7 @@ class DnnAndVit:
         """vit = Viterbi(A=torch.from_numpy(self.Transition_Matrix), P=torch.from_numpy(self.P_Matrix_probs),
         logscale=self.logscale, alpha=self.alpha, print_info=self.print_info) return vit.x.numpy()"""
         vit = Viterbi(A=self.Transition_Matrix, P=self.P_Matrix_probs, logscale=self.logscale, alpha=self.alpha,
-                      print_info=self.print_info)
+                      print_info=self.print_info, softmax=self.softmax)
         if torch.is_tensor(self.Transition_Matrix):
             return vit.x, vit.T1, vit.y
         else:

Optimize_Transmatrix.py
@@ -130,7 +130,7 @@ class OptimTransMatrix:
         row_sums = torch.sum(trans, dim=1) # normalize transition matrix
         row_sums = row_sums[:,None]
         normalized_trans_matr = torch.div(trans, row_sums)
-        res = Viterbi(normalized_trans_matr, data, alpha=self.alpha, logscale=True, return_log=True, print_info=False)
+        res = Viterbi(normalized_trans_matr, data, alpha=self.alpha, logscale=True, return_log=True, print_info=False, softmax=self.softmax)
         if self.use_normalized:
             res_normalized = torch.exp(res.T1)
             row_sums = torch.sum(res_normalized, dim=0)  # normalize transition matrix

Viterbi/Viterbi_Algorithm.py
@@ -29,7 +29,7 @@ class Viterbi:
     """
 
 
-    def __init__(self, A, P, Pi=None, logscale=True, alpha=None, return_log=None, print_info=True):
+    def __init__(self, A, P, Pi=None, logscale=True, alpha=None, return_log=None, print_info=True, softmax=False):
         # check if the input is a torch tensor or a numpy array
         if torch.is_tensor(A):
             self.is_torch = True
@@ -43,6 +43,7 @@ class Viterbi:
         self.P = P
         self.logscale = logscale
         self.print_info = print_info
+        self.softmax = softmax
 
         # Initialize the return_log variable
         if return_log is None:
@@ -100,7 +101,8 @@ class Viterbi:
         if self.is_torch:
             T1 = torch.empty((self.K, self.T), dtype=torch.float64, device=self.device)
             T2 = torch.empty((self.K, self.T), dtype=torch.int, device=self.device)
-            T3 = torch.zeros((self.K, self.T), dtype=torch.float64, device=self.device)
+            if self.softmax:
+                T3 = torch.zeros((self.K, self.T), dtype=torch.float64, device=self.device)
         else:
             T1 = np.empty((self.K, self.T), 'd')
             T2 = np.empty((self.K, self.T), 'B')
@@ -124,8 +126,9 @@ class Viterbi:
                     T1[:, i] = torch.max(
                         T1[:, i - 1] + 2 * self.alpha * self.A.T + 2 * (1 - self.alpha) * (self.P[None, i]).T, 1).values
                     T2[:, i] = torch.argmax(T1[:, i - 1] + 2 * self.alpha * self.A.T, 1)
-                    temp = torch.nn.functional.softmax(T1[:, i - 1] + 2 * self.alpha * self.A.T, 1)
-                    T3[:, i] = torch.matmul(temp, torch.arange(self.K, dtype=torch.float64)[:,None]).squeeze(1)
+                    if self.softmax:
+                        temp = torch.nn.functional.softmax(T1[:, i - 1] + 2 * self.alpha * self.A.T, 1)
+                        T3[:, i] = torch.matmul(temp, torch.arange(self.K, dtype=torch.float64)[:,None]).squeeze(1)
 
                 else:
                     T1[:, i] = np.max(
@@ -155,15 +158,16 @@ class Viterbi:
             y = torch.empty(self.T, dtype=torch.float64, device=self.device)
             # x = torch.empty(self.T, dtype=torch.float64, device=self.device)
             x[-1] = torch.argmax(T1[:, self.T - 1])
-            temp = torch.nn.functional.softmax(T1[:, self.T - 1], dim=0)
-            y[-1] = torch.matmul(temp, torch.arange(self.K, dtype=torch.float64)[:,None])
+            if self.softmax:
+                temp = torch.nn.functional.softmax(T1[:, self.T - 1], dim=0)
+                y[-1] = torch.matmul(temp, torch.arange(self.K, dtype=torch.float64)[:,None])
         else:
             x = np.empty(self.T, 'B')
             x[-1] = np.argmax(T1[:, self.T - 1])
 
         for i in reversed(range(1, self.T)):
             x[i - 1] = T2[x[i], i]
-            if self.is_torch:
+            if self.is_torch and self.softmax:
                 y[i - 1] = T3[x[i], i]
 
         if self.logscale != self.return_log:
@@ -179,6 +183,8 @@ class Viterbi:
                     T1 = np.exp(T1)
 
         if self.is_torch:
+            if not self.softmax:
+                T3, y = None, None
             return x, T1, T2, T3, y
         else:
             return x, T1, T2