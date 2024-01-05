e0e1e0f13ccc64eb86627e526197a57efeb557e0
jj <jakob.scheytt@gmx.de>
2024-01-05 16:19:11 +0100

    New test file for testing savetxt

--- test_savetxt.py
+++ test_savetxt.py
@@ -1,4 +1,5 @@
 import numpy as np
 
-out_name = ("./Probability_Data/")
-np.savetxt('test_data.txt', X=[0, 1, 2, 3], fmt="%d", delimiter=",")
+out_name = ("./Probability_Data/Sleep-EDF-2018-val/")
+#out_name = ("./")
+np.savetxt(out_name + 'test_data.txt', X=[0, 1, 2, 3, 4], fmt="%d", delimiter=",")
