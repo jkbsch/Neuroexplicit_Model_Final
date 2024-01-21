d650fe492ad15646494fd38912bfe26d25c96410
jj <jakob.scheytt@gmx.de>
2024-01-21 10:10:50 +0100

    Attention message removed

--- HMM_utils.py
+++ HMM_utils.py
@@ -84,7 +84,6 @@ def set_dataset(used_set, dataset, trans_matrix):
             end_fold = 20
             end_nr = 2
             leave_out = [(5, 13), (6, 13), (7, 13), (9, 13)]
-            print("[ATTENTION]: val set must be checked")
     elif dataset == 'Sleep-EDF-2018':
         if used_set == 'test':
             end_fold = 10
@@ -98,7 +97,6 @@ def set_dataset(used_set, dataset, trans_matrix):
             end_fold = 10
             end_nr = 13
             leave_out = [(5, 13), (6, 13), (7, 13), (9, 13)]
-            print("[ATTENTION]: val set must be checked")
     else:
         print("[INFO]: Dataset is not or incorrectly defined. Dataset is set to Sleep-EDF-2013")
         dataset = 'Sleep-EDF-2013'
