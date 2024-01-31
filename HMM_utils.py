1b22f4a47b204ab9ff9159cbcc70c2e0f5b29dfe
jj <jakob.scheytt@gmx.de>
2024-01-31 10:26:33 +0100

    Added comments

--- HMM_utils.py
+++ HMM_utils.py
@@ -7,8 +7,14 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
 
+# loads the transition matrix. If check = True, it only checks whether the name of the transition matrix is correct.
 def load_Transition_Matrix(trans_matr="EDF-2018", oalpha=False, otrans=False, fold=1, check=False,
                            successful=True, checkpoints='given', lr=0.00001, alpha=0.3, epochs=60, FMMIE=None, mlength=None, trwval=None, startalpha=None):
+    """loads the transition matrix. If check = True, it only checks whether the name of the transition matrix is correct.
+    If other datasets are used, make sure to adjust this function accordingly
+    """
+
+    # checks different ways of defining the transition matrix
     if (trans_matr == "edf-2013-and-edf-2018" or trans_matr == 'EDF 2013 and 2018' or trans_matr == '2013 2018' or
             trans_matr == 'Sleep-EDF-2013-And-Sleep-EDF-2018' or trans_matr == 'edf_2013_and_edf_2018'):
         trans_matr = "Sleep-EDF-2013-And-Sleep-EDF-2018"
@@ -19,6 +25,7 @@ def load_Transition_Matrix(trans_matr="EDF-2018", oalpha=False, otrans=False, fo
           trans_matr == 'Sleep_EDF_2018'):
         trans_matr = 'Sleep-EDF-2018'
     else:
+        # if the transition matrix is not correctly defined, False is returned
         return False
 
     if check:
@@ -42,16 +49,20 @@ def load_Transition_Matrix(trans_matr="EDF-2018", oalpha=False, otrans=False, fo
     transitionmatrix = np.loadtxt(Trans_path, delimiter=",")
 
     if np.shape(transitionmatrix) == (6, 5):
+        # if alpha was trained, its value is stored in the first row of the transition matrix. It is removed here
         alpha = transitionmatrix[0, 0]
         transitionmatrix = transitionmatrix[1:, :]
     else:
         alpha = None
 
-    transitionmatrix[transitionmatrix <= 0] = float(1e-10)  # negative or 0 Werte werden sehr klein gesetzt
+    # transitionmatrix[transitionmatrix <= 0] = float(1e-10)
     return alpha, transitionmatrix
 
 
 def load_P_Matrix(checkpoints='given', dataset='Sleep-EDF-2013', used_set='train', fold=1, nr=0, print_info=True):
+    """
+    This functions loads the P_Matrix (labels and probabilities) for the given dataset, fold and nr.
+    """
     if checkpoints == "given" or checkpoints == "Given":
         chkpt = ""
     elif checkpoints == "own" or checkpoints == "Own":
@@ -70,11 +81,18 @@ def load_P_Matrix(checkpoints='given', dataset='Sleep-EDF-2013', used_set='train
     return P_Matrix_labels, P_Matrix_probs
 
 
-def pure_predictions(P_Matrix_probs):
+def pure_predictions(P_Matrix_probs): # calculates the pure predictions from the probabilities by applying the argmax
     return np.argmax(P_Matrix_probs, axis=1)
 
 
 def set_dataset(used_set, dataset, trans_matrix):
+    """
+    This function can be seen as config file. The final fold and nr differ for every dataset and fold, so some fold-number
+    combinations have to be excluded. Make sure to adjust this function when using other datasets.
+    end_fold: number of total folds for this dataset
+    end_nr: number of nights for every fold in this set
+    leave_out: fold-number combinations that are excluded
+    """
     if dataset == 'Sleep-EDF-2013':
         if used_set == 'test':
             end_fold = 20
@@ -118,7 +136,7 @@ def set_dataset(used_set, dataset, trans_matrix):
             leave_out = [()]
             print("[ATTENTION]: val set must be checked")
 
-    if load_Transition_Matrix(trans_matrix, check=True):
+    if load_Transition_Matrix(trans_matrix, check=True): # load transition matrix and check for errors
         return dataset, trans_matrix, end_fold, end_nr, leave_out
     elif trans_matrix is not None:
         print("[INFO]: 'transmatrix' was not (correctly) defined. It is set to the one according to the dataset")
@@ -132,16 +150,22 @@ def set_dataset(used_set, dataset, trans_matrix):
 
 # adapted from SleePyCo
 def summarize_result(config, fold, y_true, y_pred, save=True):
+    """
+    Summarizes the result by calculating the accuracy, macro f1, kappa, precision, recall and f1 for every class, and
+    plots the confusion matrix. This function is called by Optimize_Alpha and used when evaluate_result=True.
+    In order to properly print the output on Windows with Pycharm, make sure to use the option "Emulate terminal in Output Console"
+    in the Run/Debug configurations.
+    """
 
     if config["oalpha"]:
         alpha = config["all_alphas"]
     else:
         alpha = config["alpha"]
 
-    os.makedirs('../results_n', exist_ok=True)
+    os.makedirs('../results', exist_ok=True) # create results ordner if it does not exist
     result_dict = skmet.classification_report(y_true, y_pred, digits=3, output_dict=True)
     cm = skmet.confusion_matrix(y_true, y_pred)
-    print(cm)
+
 
     accuracy = round(result_dict['accuracy'] * 100, 1)
     macro_f1 = round(result_dict['macro avg']['f1-score'] * 100, 1)
@@ -171,7 +195,7 @@ def summarize_result(config, fold, y_true, y_pred, save=True):
         ['ACC', 'MF1', '\u03BA'],
         [accuracy, macro_f1, kappa],
     ]
-
+    # show the results for every class
     perclass_data = [
         [colored('A', 'cyan') + '\\' + colored('P', 'green'), 'W', 'N1', 'N2', 'N3', 'R', 'PR', 'RE', 'F1'],
         ['W', cm[0][0], cm[0][1], cm[0][2], cm[0][3], cm[0][4], wpr, wre, wf1],
@@ -181,6 +205,7 @@ def summarize_result(config, fold, y_true, y_pred, save=True):
         ['R', cm[4][0], cm[4][1], cm[4][2], cm[4][3], cm[4][4], rpr, rre, rf1],
     ]
 
+    #plot confusion matrix
     confusion_matrix = np.divide(cm, (np.sum(cm, 1)).reshape(5, 1))
     fig, ax = plt.subplots()
     ax.matshow(confusion_matrix, cmap='Blues')
@@ -195,11 +220,8 @@ def summarize_result(config, fold, y_true, y_pred, save=True):
             else:
                 ax.text(i, j, str(f'{c:.1f}%'), va='center', ha='center', color='cornsilk')
     plt.title('Predicted Class', fontsize=10)
-    # plt.xaxis.set_label_position('top')
-    # set x label to the top of the plot
 
     plt.ylabel('Actual Class')
-    # ax.set_title('Confusion Matrix')
     plt.figtext(0.01, 0.01,f' \nDataset: {config["dataset"]} Transition Matrix: {config["transmatrix"]}, Set: {config["set"]}, trained alpha: {config["oalpha"]}, trained Transition Matrix: {config["otrans"]}, Alpha: {alpha}, \ncheckpoints: {config["checkpoints"]}, epochs: {config["epochs"]}, lr: {config["lr"]}, maxlength: {config["max_length"]}, FMMIE: {config["FMMIE"]}, mlength: {config["mlength"]}, trwval: {config["trwval"]}, startalpha: {config["startalpha"]}', fontsize=6)
 
     plt.show()
@@ -208,8 +230,7 @@ def summarize_result(config, fold, y_true, y_pred, save=True):
             alpha = alpha[0]
 
     fig.savefig(
-        # f'results_n/ConfusionMatrix_Dataset: {config["dataset"]} Transition Matrix: {config["transmatrix"]}, Set: {config["set"]}, trained alpha: {config["oalpha"]}, trained Transition Matrix: {config["otrans"]}, Alpha: {alpha}, checkpoints: {config["checkpoints"]}, epochs: {config["epochs"]}, lr: {config["lr"]}, maxlength: {config["max_length"]}, FMMIE: {config["FMMIE"]}, mlength: {config["mlength"]}, trwval: {config["trwval"]}, startalpha: {config["startalpha"]}.png', dpi=1200)
-        f'results_n/ConfusionMatrix_Ds{config["dataset"][-1]}TM{config["transmatrix"][-1]}{config["set"]}oa{config["oalpha"]:0}ot{config["otrans"]:0}a{alpha:.2f}{config["checkpoints"]}e{config["epochs"]}lr{config["lr"]}maxlen{config["max_length"]}FMMIE{config["FMMIE"]:0}mlen{config["mlength"]}trw{config["trwval"]:0}sa{config["startalpha"]}.png',dpi=1200)
+        f'results/ConfusionMatrix_Ds{config["dataset"][-1]}TM{config["transmatrix"][-1]}{config["set"]}oa{config["oalpha"]:0}ot{config["otrans"]:0}a{alpha:.2f}{config["checkpoints"]}e{config["epochs"]}lr{config["lr"]}maxlen{config["max_length"]}FMMIE{config["FMMIE"]:0}mlen{config["mlength"]}trw{config["trwval"]:0}sa{config["startalpha"]}.png',dpi=1200)
 
     overall_dt = SingleTable(overall_data, colored('OVERALL RESULT', 'red'))
     perclass_dt = SingleTable(perclass_data, colored('PER-CLASS RESULT', 'red'))
@@ -221,7 +242,7 @@ def summarize_result(config, fold, y_true, y_pred, save=True):
     print(colored(' A', 'cyan') + ': Actual Class, ' + colored('P', 'green') + ': Predicted Class' + '\n\n')
 
     if save:
-        with open(os.path.join(f'results_n/numbers_Ds{config["dataset"][-1]}TM{config["transmatrix"][-1]}{config["set"]}oa{config["oalpha"]:0}ot{config["otrans"]:0}a{alpha:.2f}{config["checkpoints"]}e{config["epochs"]}lr{config["lr"]}maxlen{config["max_length"]}FMMIE{config["FMMIE"]:0}mlen{config["mlength"]}trw{config["trwval"]:0}sa{config["startalpha"]}'+ '.txt'),
+        with open(os.path.join(f'results/numbers_Ds{config["dataset"][-1]}TM{config["transmatrix"][-1]}{config["set"]}oa{config["oalpha"]:0}ot{config["otrans"]:0}a{alpha:.2f}{config["checkpoints"]}e{config["epochs"]}lr{config["lr"]}maxlen{config["max_length"]}FMMIE{config["FMMIE"]:0}mlen{config["mlength"]}trw{config["trwval"]:0}sa{config["startalpha"]}'+ '.txt'),
                   'w') as f:
             f.write(
                 str(fold) + ' ' +
@@ -238,13 +259,14 @@ def summarize_result(config, fold, y_true, y_pred, save=True):
     return [accuracy, macro_f1, kappa, wpr, wre, wf1, n1pr, n1re, n1f1, n2pr, n2re, n2f1, n3pr, n3re, n3f1, rpr, rre, rf1, confusion_matrix]
 
 
-def posteriogram(y_true, y_pred,sleepy_pred, config, xmin=400, xmax=450):
+def plot_entire_night(y_true, y_pred, sleepy_pred, config, xmin=400, xmax=450):
+    # plots the predictions for the entire night and a section (defined by xmin and xmax), comparing the predictions of
+    # SleePyCo, the hybrid model and labels
     length = len(y_true)
     X = np.arange(length)
     fig, ax = plt.subplots(2, 1)
 
-    # fig.suptitle('Comparison Hybrid and Labels')
-    # plt.figure(figsize=(600, 100))
+
     ax[1].set_xlim(xmin, xmax)
     ax[0].set_xlim(0, length)
     y_true = y_true * -1
@@ -259,7 +281,6 @@ def posteriogram(y_true, y_pred,sleepy_pred, config, xmin=400, xmax=450):
 
     ax[0].step(X, y_true, color='black')
     ax[0].scatter(X, np.where(y_true != y_pred, y_pred, None), color='red', s=6)
-    # ax[0].scatter(X, np.where(y_true != y_pred, y_true, None), color='green', s=6)
     ax[0].scatter(X, np.where(y_pred != sleepy_pred, sleepy_pred, None), color='indigo', s=6)
     ax[0].set_ylim(-4.5, 0.5)
     ax[0].set_yticks([0, -1, -2, -3, -4], ["W", "N1", "N2", "N3", "REM"])
@@ -274,12 +295,15 @@ def posteriogram(y_true, y_pred,sleepy_pred, config, xmin=400, xmax=450):
     description = f'Predictions for: Dataset: {config["dataset"]} Set: {config["used_set"]} Fold: {config["fold"]} Nr: {config["nr"]} \nTransition Matrix: {config["trans_matrix"]} Alpha: {alpha} trained Transition: {config["otrans"]}, trained Alpha: {config["oalpha"]}, checkpoints: {config["checkpoints"]}, maxlength: {config["max_length"]}, FMMIE: {config["FMMIE"]}, mlength: {config["mlength"]}, trwval: {config["trwval"]}, startalpha: {config["startalpha"]}'
     plt.figtext(0.1, 0.01, description, fontsize=6)
     plt.show()
-    fig.savefig(f'results_n/figure_posteriogram_Ds{config["dataset"][-1]}TM{config["trans_matrix"][-1]}{config["used_set"]}oa{config["oalpha"]:0}ot{config["otrans"]:0}a{alpha:.2f}{config["checkpoints"]}e{config["epochs"]}lr{config["lr"]}maxlen{config["max_length"]}FMMIE{config["FMMIE"]:0}mlen{config["mlength"]}trw{config["trwval"]:0}sa{config["startalpha"]}{xmin, xmax}.png', dpi=1200)
+    fig.savefig(f'results/figure_posteriogram_Ds{config["dataset"][-1]}TM{config["trans_matrix"][-1]}{config["used_set"]}oa{config["oalpha"]:0}ot{config["otrans"]:0}a{alpha:.2f}{config["checkpoints"]}e{config["epochs"]}lr{config["lr"]}maxlen{config["max_length"]}FMMIE{config["FMMIE"]:0}mlen{config["mlength"]}trw{config["trwval"]:0}sa{config["startalpha"]}{xmin, xmax}.png', dpi=1200)
 
 
-def visualize_probs(y_true, probs_hybrid, probs_sleepy, y_pred_sleepy, y_pred_hybrid, config, xmin=400, xmax=440):
+def posteriogram(y_true, probs_hybrid, probs_sleepy, y_pred_sleepy, y_pred_hybrid, config, xmin=400, xmax=440):
+    """
+    Plots the posteriogram and the delta / T1 matrix for the given section (defined by xmin and xmax) and displays correct and
+    incorrect predictions
+    """
     fig, ax = plt.subplots(2, 1)
-    #fig.suptitle('Comparison of Labels, Hybrid and Pure Predictions')
 
     probs_sleepy = probs_sleepy[xmin:xmax]
     X = np.arange(xmax - xmin)
@@ -319,14 +343,16 @@ def visualize_probs(y_true, probs_hybrid, probs_sleepy, y_pred_sleepy, y_pred_hy
     description = f'\n Dataset: {config["dataset"]} Set: {config["used_set"]} Fold: {config["fold"]} Nr: {config["nr"]} Transition Matrix: {config["trans_matrix"]} Alpha: {alpha:.3f}, checkpoints: {config["checkpoints"]}, \n trained Transition: {config["otrans"]}, trained Alpha: {config["oalpha"]}, maxlength: {config["max_length"]}, FMMIE: {config["FMMIE"]}, mlength: {config["mlength"]}, trwval: {config["trwval"]}, startalpha: {config["startalpha"]}'
     plt.figtext(0.1, 0.01, description, wrap=True, fontsize=6)
     plt.show()
-    fig.savefig(f'results_n/figure_probs_Ds{config["dataset"][-1]}TM{config["trans_matrix"][-1]}{config["used_set"]}oa{config["oalpha"]:0}ot{config["otrans"]:0}a{alpha:.2f}{config["checkpoints"]}e{config["epochs"]}lr{config["lr"]}maxlen{config["max_length"]}FMMIE{config["FMMIE"]:0}mlen{config["mlength"]}trw{config["trwval"]:0}sa{config["startalpha"]}{xmin, xmax}.png', dpi=1200)
+    fig.savefig(f'results/figure_probs_Ds{config["dataset"][-1]}TM{config["trans_matrix"][-1]}{config["used_set"]}oa{config["oalpha"]:0}ot{config["otrans"]:0}a{alpha:.2f}{config["checkpoints"]}e{config["epochs"]}lr{config["lr"]}maxlen{config["max_length"]}FMMIE{config["FMMIE"]:0}mlen{config["mlength"]}trw{config["trwval"]:0}sa{config["startalpha"]}{xmin, xmax}.png', dpi=1200)
 
 def visualize_alphas():
+    """
+    This function plots the accuracies for different alphas. The alphas first have to be calculated by the optimize_alpha
+    function and then, they have to be stored (saving the results is not implemented in this version).
+    Make sure to adjust the input path
+    """
     alphas = np.loadtxt('results/new_alphas_notrain_exact_maxlength10_step0.05.txt', delimiter=',')
     accuracies = np.loadtxt('results/new_accuracies_notrain_exact_maxlength10_step0.05.txt', delimiter=',')
-
-    """fig, ax = plt.subplots(2,2)
-    length = len(alphas[0])"""
     fig, ax = plt.subplots()
 
     fig.suptitle('Alphas and respective Accuracies, predictions for N = 10')
@@ -338,10 +364,6 @@ def visualize_alphas():
     plt.xlim((0, 2))
     plt.ylim((82.5, 92.5))
 
-    """for i, dataset in enumerate(['Sleep-EDF-2013', 'Sleep-EDF-2018']):
-        for j, used_set in enumerate(['train', 'test']):
-            ax[i][j].scatter(alphas[i][:-4], accuracies[j][:-4])
-            ax[i][j].set_title(f'Dataset: {dataset}, set: {used_set}')"""
 
     plt.xlabel('alpha')
     plt.ylabel('Accuracy in %')
@@ -350,20 +372,25 @@ def visualize_alphas():
     plt.show()
 
     fig.tight_layout()
-    fig.savefig(f'results_n/new_Comparing alphas_untrained_unlimited_long.png', dpi=1200)
+    fig.savefig(f'results/new_Comparing alphas_untrained_unlimited_long.png', dpi=1200)
 
 def analyze_errors(y_true, hybrid_pred,sleepy_pred):
+    """
+    This function analyzes the errors depending on the context in which they were made
+    """
     length = len(y_true)
     nr_same_epochs = 7
 
+    # errors_long_hybrid: counts the errors for the hybrid model and SleePyCo in a long and constant phase (defined by nr_same_epochs)
     y_true = np.array(y_true)
     hybrid_pred = np.array(hybrid_pred)
     sleepy_pred = np.array(sleepy_pred)
-    errors_long_hybrid = np.zeros(5, dtype=int) #zählt folgenden Fehler: wenn in einer langen Phase immer gleiche Schlafphasen sind, wie oft liegt dann das jeweilige Modell falsch?
+    errors_long_hybrid = np.zeros(5, dtype=int)
     errors_long_sleepy = np.zeros(5, dtype=int)
     nr_long = 0
 
-    errors_single_hybrid = np.zeros(5, dtype=int) # zählt folgenden Fehler: wenn in einer langen Phase eine einzige Epoche anders ist, erkennt es das jeweilige System?
+    # errors_single_hybrid: counts the errors for the hybrid model in a long and constant phase (defined by nr_same_epochs), if the middle epoch differs
+    errors_single_hybrid = np.zeros(5, dtype=int)
     errors_single_sleepy = np.zeros(5, dtype=int)
     nr_single = 0
 
@@ -396,6 +423,9 @@ def analyze_errors(y_true, hybrid_pred,sleepy_pred):
     nr_changing = 20
     threshold = 4
 
+    # errors_fast_changing counts the number of errors SleePyCo / the hybrid model makes in a phase of length
+    # nr_changing, if the number of changes is higher than threshold
+
     for i in range(length-nr_changing):
         count_changes = 0
         for j in range(nr_changing-1):
@@ -412,6 +442,3 @@ def analyze_errors(y_true, hybrid_pred,sleepy_pred):
 
     return errors_long_hybrid, errors_long_sleepy, errors_single_hybrid, errors_single_sleepy, nr_long, nr_single, errors_fast_changing_hybrid, errors_fast_changing_sleepy, nr_fast
 
-
-
-# visualize_alphas()
